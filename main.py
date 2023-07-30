489ff58839fdb5a918a4a63ebf6fd40a4d6ee918
blackdocs <aram@blackdocs.cloud>
2023-07-30 22:07:37 +0400

    create app

---
+++ main.py
@@ -0,0 +1,29 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/api/healthz')
+def healthz(request: Request):
+    return templates.TemplateResponse("healthz.html", {"request": request})
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/en")
+
+@app.get("/{language}")
+def home(request: Request, language: str):
+    valid_languages = ["en", "am", "ru"]
+    if language not in valid_languages:
+        return RedirectResponse(url="/en")
+
+    return templates.TemplateResponse(f"index_{language}.html", {"request": request})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8888)
